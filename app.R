2f9f60191dbfa63e71f9d055b68d41dc2a58b1cf
hurrialice <zhangq07@yahoo.com>
2019-01-10 23:50:17 -0500

    skeleton

--- app.R
+++ app.R
@@ -1,118 +1,94 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
-
 library(shiny)
 source("lib.R")
 
-# Options
-DEFAULT_SD = 10
-DEFAULT_N  = 10
 
-# Define UI for application that draws a histogram
-ui <- fluidPage(
-   
-   # Application title
-   titlePanel("Imbalanced"),
-   
-   # Sidebar with a slider input for number of bins 
-   sidebarLayout(
-      sidebarPanel(
-          shiny::div(id="dynControlUI")
-      ),
-      
-      # Show a plot of the generated distribution
-      mainPanel(
-         plotOutput("pointsPlot", click = "pointsPlot_click", dblclick = "pointsPlot_dblclick")
-      )
-   )
-)
 
-loadControlUI <- function(suffix, label) {
-    tagList(
-        shiny::sliderInput(inputId = paste0("SDInput", suffix), label = paste(label, "SD"), value = DEFAULT_SD, min = 1, max = 50, step = 1),
-        shiny::sliderInput(inputId = paste0("NumInput", suffix), label = paste(label, "N"), value = DEFAULT_N, min = 1, max = 100, step = 1)
-    )
+# For dropdown menu
+actionLink <- function(inputId, ...) {
+    tags$a(href='javascript:void',
+           id=inputId,
+           class='action-button',
+           ...)
 }
 
+ui <- fixedPage(
+    titlePanel("Handling the Imbalanced Data"),
+    h5("Qing Zhang, CBQG@HSPH"),
+    h5(paste0("Real data, compared to the ones we used in ",
+              "class, are usually messy and *imbalanced*,",
+              "The problem may or may not pose an issue, ", 
+              "depending on the specific problem.",
+              "Here we present an user interface to simulate imbalanced data, ",
+              "together with several techniques to leverage ",
+              "this issue, including model selection and resampling ",
+              "to see how classification performance will be changed. ")),
+    
+    br(),
+    
+    fluidRow(
+        column(4,
+               wellPanel(
+                   
+                   # data generation
+                   h4("Set your learning data"),
+                   sliderInput("bg_ratio", "True label distribution ratio",
+                               0.05, 1, 0.5, step = 0.05),
+                   sliderInput("c1r", "class 1 sample ratio", 0.05, 1, 0.4, step=0.05),
+                   sliderInput("c2r","class 2 sample ratio", 0.05, 1, 0.1, step = 0.05)
+               ),
+               wellPanel(
+                   
+                   h4("Choose your classifier"),
+                   
+                   # classification method
+                   selectInput("class_method", "Classifier",
+                               choices = c(Random_forest = "rf", 
+                                           Generalized_Linear_Models = "glm",
+                                           Linear_discriminant_analysis = "lda",
+                                           Decision_Tree = "rpart",
+                                           K_Nearest_Neighbor = "knn")),
+                   
+                   h4("Choose your resample scheme"),
+                   radioButtons("sample_method", "How do you deal with class imbalance?",
+                                choices = c("Oversample", 'Undersample', "As-is"),
+                                selected = "As-is"),
+                   
+                   tags$hr(),
+                   
+                   tags$small(paste0(
+                       "Note: The bold symbols in the decision plot are the ones selected",
+                       " (as chosen by the class x sample ratio). You may change the algorithm",
+                       " or the resampling scheme and determine whichever gives better prediction."
+                   ))
+               )
+        ),
+        column(7,
+               wellPanel(h5("Decision Plot"),
+                         plotOutput("decision_bounds"),
+                         tags$hr(),
+                         h5("Performance Measures (When each class is defined as positive)"),
+                         plotOutput("performance")
+               )
+        )
+    )
+)
 
 
 server <- function(input, output) {
-    app.states = reactiveValues(points = list())
     
-    observeEvent(input$pointsPlot_dblclick, {
-        app.states$points <- list()
-        removeUI("div:has(> #SDInput1)")
-        removeUI("div:has(> #SDInput2)")
-        removeUI("div:has(> #SDInput3)")
-        removeUI("div:has(> #SDInput4)")
-        removeUI("div:has(> #SDInput5)")
-        removeUI("div:has(> #NumInput1)")
-        removeUI("div:has(> #NumInput2)")
-        removeUI("div:has(> #NumInput3)")
-        removeUI("div:has(> #NumInput4)")
-        removeUI("div:has(> #NumInput5)")
+    model <- reactive({
+        l_allpoints <- do.call("l_make_points", make_centroid(bg_ratio = input$bg_ratio))
+        l_selectedpoints <- l_select_points(l_allpoints, c(input$c1r, input$c2r))
+        l_resample <- resample_train(method = input$sample_method, l_selectedpoints)
+        make_model(l_resample, method = input$class_method)
     })
+    print("a")
+    output$decision_bounds <- renderPlot(plot_points(l_allpoints, l_resample, model()))
     
-    control_observer <- function(id) {
-        id <- as.integer(id)
-        bquote({
-            print(paste("wtf", .(id)))
-            sd <- input[[paste0("SDInput", .(id))]]
-            n  <- input[[paste0("NumInput", .(id))]]
-            
-            opts <- attr(app.states$points[[.(id)]], "opts")
-            if (n == opts$n && sd == opts$sd)
-                return()
-            app.states$points[[.(id)]] <- make_points(cx = opts$cx, cy = opts$cy, label = opts$label, n = n, sd = sd)
-        })
-    }
-    
-    nullnull <- function(a, b) {
-        if (is.null(a) || is.null(b))
-            return(NULL)
-        TRUE
-    }
-    observeEvent( nullnull(input$SDInput1, input$NumInput1) , control_observer(1), handler.quoted = TRUE, ignoreInit = TRUE, ignoreNULL = TRUE)
-    observeEvent( nullnull(input$SDInput2, input$NumInput2) , control_observer(2), handler.quoted = TRUE, ignoreInit = TRUE, ignoreNULL = TRUE)
-    observeEvent( nullnull(input$SDInput3, input$NumInput3) , control_observer(3), handler.quoted = TRUE, ignoreInit = TRUE, ignoreNULL = TRUE)
-    observeEvent( nullnull(input$SDInput4, input$NumInput4) , control_observer(4), handler.quoted = TRUE, ignoreInit = TRUE, ignoreNULL = TRUE)
-    observeEvent( nullnull(input$SDInput5, input$NumInput5) , control_observer(5), handler.quoted = TRUE, ignoreInit = TRUE, ignoreNULL = TRUE)
-    
-    observeEvent(input$pointsPlot_click, {
-        if (length(app.states$points) >= 5) {
-            return()
-        }
-        
-        cen.x1 <- input$pointsPlot_click$x
-        cen.x2 <- input$pointsPlot_click$y
-        
-        new_label <- {
-            existing_labels <- sapply(app.states$points, function(p) {
-                ans <- unique(p$y)
-                stopifnot(length(ans) == 1)
-                ans
-            })
-            LETTERS[!LETTERS %in% existing_labels][1]
-        }
-        
-        app.states$points[[length(app.states$points) + 1]] <- make_points(cx = cen.x1, cy = cen.x2, label = new_label,
-                                                                          sd = DEFAULT_SD, n = DEFAULT_N)
-        insertUI("#dynControlUI", where = "beforeEnd", ui = {
-            loadControlUI(suffix = length(app.states$points), label = new_label)
-        })
-    })
-    
-    output$pointsPlot <- renderPlot(width = 500, height = 500, {
-        plot_points(app.states$points, make_model(app.states$points))
-    })
+    # plot performance
+    output$performance <- renderPlot(plot_perf(measure_perf(model(), l_allpoints)))
 }
+
 
 # Run the application 
 shinyApp(ui = ui, server = server)
-
--- lib.R
+++ lib.R
@@ -1,8 +1,35 @@
 library(caret)
 library(ggplot2)
 library(tibble)
+library(tidyr)
+###### Data #####
 
-make_points <- function(cx, cy, label, n = 10, sd = 10) {
+
+# make evenly spread centroids from the number of class
+
+
+
+# ! only two classes
+make_centroid <- function(ncen = 2, bg_ratio){
+    
+    cx = c(1,3)
+    cy = c(3,1)
+    
+    # calculate points number for each class
+    bgn = 300
+    c1 = round(bgn*bg_ratio)
+    c2 = bgn - c1
+    
+    return(list(cx = cx, cy = cy, label=LETTERS[1:ncen], n = c(c1, c2)))
+}
+
+
+
+do.call("l_make_points", make_centroid(bg_ratio = 0.2))
+
+# make background points
+
+make_points <- function(cx, cy, label, n = 10, sd = 2) {
     stopifnot(length(cx) == 1)
     stopifnot(length(cy) == 1)
     stopifnot(length(label) == 1)
@@ -18,13 +45,15 @@ make_points <- function(cx, cy, label, n = 10, sd = 10) {
     ans
 }
 
-l_make_points <- Vectorize(make_points, vectorize.args = c("cx", "cy", "label", "n"), SIMPLIFY = FALSE)
 
-if (FALSE) {
-    make_points(1, 3, label = "wtf", n = 13)
-    a <- l_make_points(1:3, 3:1, label = "wtf")
-    a
-    do.call(rbind, a)
+
+# select points
+
+select_points <- function(df, select_ratio){
+    nrows <- nrow(df)
+    df_select <- df[sample(seq(nrows), size  = round(select_ratio*nrows)), ]
+    attr(df_select, "opts")  <- c(attr(df, "opts"), list(sratio = select_ratio))
+    return (df_select)
 }
 
 merge_points <- function(ldf) {
@@ -32,11 +61,35 @@ merge_points <- function(ldf) {
         ans <- data.frame(x1 = numeric(), x2 = numeric(), y = character(), stringsAsFactors = FALSE)
     else
         ans <- do.call(rbind, ldf)
-    
+    ## print(ans)
     ans[['y']] <- forcats::as_factor(ans[['y']])
     ans
 }
-make_model <- function(lst.points, method = c("rf")) {
+
+
+# for a single centroid, first generate the background assumptions, the true label distribution,
+# and then the select portion between labels.
+
+l_make_points <- Vectorize(make_points, vectorize.args = c("cx", "cy", "label", "n"), SIMPLIFY = FALSE)
+l_select_points <- Vectorize(select_points, vectorize.args = c("df", "select_ratio"), SIMPLIFY = FALSE)
+
+
+
+if (FALSE) {
+    make_points(1, 3, label = "wtf", n = 13)
+    a <- l_make_points(1:3, 3:1, label = "wtf")
+    a
+    b <- l_select_points(a, c(0.2, 0.4, 0.2))
+    b
+    do.call(rbind, a)
+}
+
+
+# ************************************************************
+####### Classifiers #######
+
+# the machine learning steps
+make_model <- function(lst.points, method = c("rf","lda","rpart","knn", "glm")) {
     method <- match.arg(method)
     
     stopifnot(is.list(lst.points))
@@ -53,36 +106,51 @@ make_model <- function(lst.points, method = c("rf")) {
 }
 
 if (FALSE) {
-    data <- l_make_points(c(1:3), c(3:1), label = c("a", "b", "c"), n = c(110, 120, 130), sd = 1)
-    #data <- l_make_points(c(1:3), c(3:1), label = c("a", "b", "c"), n = c(11, 12, 13), sd = 1)
-    data <- do.call(rbind, data)
-    system.time( model <- make_model(data) )
+    data_bg <- l_make_points(c(1:3), c(3:1), label = c("a", "b", "c"), n = c(110, 120, 130), sd = 1)
+    data_select <- l_select_points(data_bg, select_ratio = c(0.1, 0.3, 0.6))
+    data_bind <- do.call(rbind, data_select)
+    system.time( model <- make_model(data_select) )
     model
 }
 
 
-plot_points <- function(lst.points, model, resolution = 100) {
-    stopifnot(is.list(lst.points))
-    stopifnot(all(sapply(lst.points, is.data.frame)))
-    points <- merge_points(lst.points)
-    stopifnot(colnames(points) == c("x1", "x2", "y"))
+####### Plots #######
+
+
+plot_points <- function(lst.points_all, lst.points_select, model, resolution = 100) {
+    #browser()
+    all_points <- merge_points(lst.points_all)
+    select_points <- merge_points(lst.points_select)
+    #stopifnot(colnames(points) == c("x1", "x2", "y"))
     
     #if (length(lst.points) < 2) {
     #    plot(points[, 1:2], col = as.integer(cl) + 1L, pch = as.integer(cl) + 1L, xlim = c(0, 100), ylim = c(0, 100))
     #    return(NULL)
     #}
     
-    decisionplot(model, data = points, resolution = resolution)
+    decisionplot(model, data_all = all_points, data_select = select_points, resolution = resolution)
 }
 
-decisionplot <- function(model, data, resolution = 100) {
+
+
+decisionplot <- function(model, data_all, data_select, resolution = 10000) {
     # Based on http://www.cmap.polytechnique.fr/~lepennec/R/Learning/Learning.html
-    cl <- data[, 3]
-    stopifnot(is.factor(cl))
-    data <- data[, 1:2]
-    k <- length(unique(cl))
+    cl_all <- data_all[, 3]
+    stopifnot(is.factor(cl_all))
+    data_all <- data_all[, 1:2]
+    k <- length(unique(cl_all))
     
-    plot(data, col = as.integer(cl) + 1L, pch = as.integer(cl) + 1L, xlim = c(0, 100), ylim = c(0, 100))
+    
+    cl_select<- data_select[, 3]
+    stopifnot(is.factor(cl_select))
+    data_select <- data_select[, 1:2]
+    #k_select <- length(unique(cl_select))
+    
+    xmax = 4
+    ymax = 4
+    
+    plot(data_all, col = as.integer(cl_all) + 1L, pch = as.integer(cl_all) + 1L, xlim = c(0, xmax), ylim = c(0, ymax))
+    points(data_select, col = as.integer(cl_select) + 1L, pch = as.integer(cl_select) + 1L, lwd = 4)
     
     if (is.null(model)) {
         return()
@@ -90,7 +158,7 @@ decisionplot <- function(model, data, resolution = 100) {
     
     # view port
     # FIXME: adjust the range
-    r <- data.frame(x1 = c(0, 100), x2 = c(0, 100))
+    r <- data.frame(x1 = c(0, xmax), x2 = c(0, ymax))
     xs <- seq(r[1,1], r[2,1], length.out = resolution)
     ys <- seq(r[1,2], r[2,2], length.out = resolution)
     g <- cbind(rep(xs, each=resolution), rep(ys, time = resolution))
@@ -103,36 +171,123 @@ decisionplot <- function(model, data, resolution = 100) {
     p <- as.factor(p)
     
     # Plot grid
-    points(g, col = as.integer(p) + 1L, pch = ".")
+    points(g, col = as.integer(p) + 1L, pch = ".", lwd = 2)
     
     
     # Plot boundary
     z <- matrix(as.integer(p), nrow = resolution, byrow = TRUE)
-    contour(xs, ys, z, add = TRUE, drawlabels = FALSE, lwd = 2, levels = (1:(k-1))+.5)
+    contour(xs, ys, z, add = TRUE, drawlabels = FALSE, lwd = 2, levels = (1:(k-1))+.5, xlab="", ylab="")
     
     invisible(z)
 }
 
 
-if (FALSE) {
-    points <- l_make_points(c(10,20,30), c(30,20,10), label = c("A", "B", "C"))
-    model <- make_model(points)
-    points <- do.call(rbind, points)
-    points$y <- forcats::as_factor(points$y)
-    decisionplot(model, data = points)
-}
+####### Performance ########
 
-if (FALSE) {
-    points <- l_make_points(c(10,20,30), c(30,20,10), label = c("A", "B", "C"))
-    plot_points(points, model = make_model(points))
-}
-
-
-if (FALSE) {
-    # Only one click
-    points <- l_make_points(c(10), c(30), label = c("A"))
-    plot_points(points, model = make_model(points))
+measure_perf <- function(model, lst.all_points){
+    
+    stopifnot(is.list(lst.all_points))
+    stopifnot(all(sapply(lst.all_points, is.data.frame)))
+    points <- merge_points(lst.all_points)
+    stopifnot(colnames(points) == c("x1", "x2", "y"))
+    
+    # browser()
+    points$y <- as.factor(points$y)
+    
+    pred <- predict(model, points)
+    
+    
+    get_mat <- function(x, y, reverse = FALSE){
+        if (reverse) {
+            x = factor(x,levels(x)[c(2,1)])
+            y = factor(y,levels(y)[c(2,1)])
+        }
+        p <- confusionMatrix(x, y)$byClass[c(1,2,5,6,7)]
+    }
+    
+    
+    cmat1 <- get_mat(pred, points$y, FALSE)
+    cmat2 <- get_mat(pred, points$y, TRUE)
+    
+    perf <- data.frame(classA = cmat1, classB = cmat2,
+                       row.names = c("Sensitivity","Specificity","Precision","Recall","F1"))
+    perf <- as.data.frame(t(perf))
+    perf$class <- gsub("class", "", rownames(perf))
+    
+    return(perf)
     
 }
+
+plot_perf <- function(perf){
+    perf <- tidyr::gather(perf, "measures", "num", -class) 
+    ggplot(perf, aes(fill=class, y=num, x=measures)) + 
+        geom_bar(position="dodge", stat="identity", alpha = 0.5) +
+        scale_fill_manual(values=c("#FF0000", "#33FF33")) +
+        theme_light() +
+        theme(legend.position="top",
+              axis.title.y= element_blank(),
+              axis.title.x= element_blank(),
+              axis.text.x = element_text(size=14))
+}
+
+
+###### Resample #######
+
+
+resample_train <- function(method, lst.points){
+    
+    nobs <- sapply(lst.points, nrow)
+    
+    if(method == "Undersample"){
+        df_resampled <- lapply(lst.points, function(df){
+            sids <- sample(seq(nrow(df)), size = min(nobs))
+            df[sids, ]
+        })
+    }
+    else if(method == "Oversample"){
+        df_resampled <- lapply(lst.points, function(df){
+            sids <- sample(seq(nrow(df)), size = max(nobs), replace = TRUE)
+            df[sids, ]
+        })
+    }
+    else{df_resampled <- lst.points}
+    
+    return(df_resampled)
+}
+
+
+
+
+
+
+##### workflow ####
+
+
+if (FALSE) {
+    
+    # set points
+    l_allpoints <- l_make_points(1:3, 3:1, n = c(100, 20, 50) , label = letters[1:3], sd=1)
+    l_selectedpoints <- l_select_points(l_allpoints, c(0.9, 0.4, 0.2))
+    
+    # classify
+    
+    ## do you resample?
+    l_resample <- resample_train(method = "Undersample", l_selectedpoints)
+    
+    ## make model
+    model <- make_model(l_resample, method = "glm")
+    
+    ## plot decisions
+    plot_points(l_allpoints, l_resample, model)
+    
+    # performance
+    perf <- measure_perf(model, l_allpoints)
+    
+    # plot performance
+    plot_perf(perf)
+    
+}
+
+
 
 
